7a3065b756c729e5d19ab92dd3e3a7dc4057a577
doprz <52579214+doprz@users.noreply.github.com>
2023-12-24 18:48:07 -0600

    feat: add Astro Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind()]
+  integrations: [solidJs(), tailwind(), sitemap()]
 });
