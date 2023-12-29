1926c34f591e1c908afd1f8e6412360b19f7ee2b
doprz <52579214+doprz@users.noreply.github.com>
2023-12-28 20:40:54 -0600

    feat: add dipc-wasm pkg files and include licenses

---
+++ dipc-wasm/pkg/dipc_wasm.d.ts
@@ -0,0 +1,47 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {number} left
+* @param {number} right
+* @returns {number}
+*/
+export function add(left: number, right: number): number;
+/**
+* @param {string} data_url
+* @param {Uint8Array} palettes
+* @returns {string}
+*/
+export function convert_data_url_to_rgba8(data_url: string, palettes: Uint8Array): string;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly add: (a: number, b: number) => number;
+  readonly convert_data_url_to_rgba8: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ dipc-wasm/pkg/dipc_wasm_bg.wasm.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function add(a: number, b: number): number;
+export function convert_data_url_to_rgba8(a: number, b: number, c: number, d: number, e: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
